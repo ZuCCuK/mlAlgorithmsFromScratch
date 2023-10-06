41511510df88185508471f34b2a994e84cbb2f1d
Ali Asaf Usta <95437193+ZuCCuK@users.noreply.github.com>
2023-10-06 23:38:59 +0300

    Update KnnFromScratch.py
    easier to use

--- KnnFromScratch.py
+++ KnnFromScratch.py
@@ -18,6 +18,7 @@ class Knn:
 
     def predict(self, new_point):
         distances = []
+        self.new_point = new_point
 
         for category in self.points:
             for point in self.points[category]:
@@ -26,37 +27,40 @@ class Knn:
         categories = [category[1] for category in sorted(distances)[:self.k]]
         result = Counter(categories).most_common(1)[0][0]
         return result
+    
+    def visualize(self):
+        fig = plt.figure(figsize=(15,12))
+        ax = fig.add_subplot(projection="3d")
+        ax.grid(True, color="#323232")
+        ax.set_facecolor("black")
+        ax.figure.set_facecolor("#121212")
+        ax.tick_params(axis="x", color="white")
+        ax.tick_params(axis="y", color="white")
+
+        for point in self.points["blue"]:
+            ax.scatter(point[0], point[1],point[2], color="#104DCA", s=60)#buraya useniom yarın yapcm
+
+        for point in self.points["red"]:
+            ax.scatter(point[0], point[1],point[2], color="#FF0000", s=60)
+
+        check = clf.predict(new_point)
+        color = "#FF0000" if check == "red" else "#104DCA"
+
+        ax.scatter(self.new_point[0], self.new_point[1],self.new_point[2], color=color, marker="*", s=200, zorder=100)
+
+        for point in points["blue"]:
+            ax.plot([self.new_point[0], point[0]], [self.new_point[1], point[1]],[self.new_point[2], point[2]], color="#104DCA", linestyle="--", linewidth=1)
+
+        for point in points["red"]:
+            ax.plot([self.new_point[0], point[0]], [self.new_point[1],point[1]],[self.new_point[2], point[2]], color="#FF0000", linestyle="--", linewidth=1)
+
+        plt.show()
+
 
 clf = Knn(3)
 clf.fit(points)
 print(clf.predict(new_point))
+clf.visualize()
 
-#ax = plt.subplot()
-fig = plt.figure(figsize=(15,12))
-ax = fig.add_subplot(projection="3d")
-ax.grid(True, color="#323232")
-ax.set_facecolor("black")
-ax.figure.set_facecolor("#121212")
-ax.tick_params(axis="x", color="white")
-ax.tick_params(axis="y", color="white")
-
-for point in points["blue"]:
-    ax.scatter(point[0], point[1], color="#104DCA", s=60)#buraya useniom yarın yapcm
-
-for point in points["red"]:
-    ax.scatter(point[0], point[1], color="#FF0000", s=60)
-
-check = clf.predict(new_point)
-color = "#FF0000" if check == "red" else "#104DCA"
-
-ax.scatter(new_point[0], new_point[1], color=color, marker="*", s=200, zorder=100)
-
-for point in points["blue"]:
-    ax.plot([new_point[0], point[0]], [new_point[1], point[1]], color="#104DCA", linestyle="--", linewidth=1)
-
-for point in points["red"]:
-    ax.plot([new_point[0], point[0]], [new_point[1], point[1]], color="#FF0000", linestyle="--", linewidth=1)
-
-plt.show()
 
 
