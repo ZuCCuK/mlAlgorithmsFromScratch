117c39a7fda539b83535527529d502e9276250ec
Ali Asaf Usta <95437193+ZuCCuK@users.noreply.github.com>
2023-10-07 00:23:27 +0300

    Update KnnFromScratch.py
    it is getting better

--- KnnFromScratch.py
+++ KnnFromScratch.py
@@ -2,9 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 
-points = {"blue": [[1,2, 4], [1,2, 3], [3,2, 3], [3, 2,4], [5, 3,4]],
-          "red": [[5, 4,6], [9,5, 6], [10,4, 5], [8,4, 6], [5,6, 6]]}
-new_point = [5, 4, 5]
+X = np.array([[2, 3,7],
+              [3, 3,1],
+              [3, 4,3],
+              [5, 4,5],
+              [5, 6,2],
+              [6, 5,3],
+              [8, 5,8],
+              [8, 6,4]])
+
+y = np.array(['blue', 'blue', 'blue', 'blue', 'red', 'red', 'red', 'red'])
+
+X_test = np.array([[2, 3,7],
+                   [7, 5,6],
+                   [1, 5, 9]])
+
 
 class Knn:
     def __init__(self, k):
@@ -13,23 +25,29 @@ class Knn:
     def getDistance(self, p, q):
         return np.sqrt(np.sum((np.array(p) - np.array(q)) ** 2))
 
-    def fit(self, points):
-        self.points = points
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
 
-    def predict(self, new_point):
-        distances = []
-        self.new_point = new_point
-
-        for category in self.points:
-            for point in self.points[category]:
+    def predict(self, X_test):
+        y_pred = []
+        
+        for new_point in X_test:
+            distances = []
+            for i, point in enumerate(self.X):
                 distance = self.getDistance(point, new_point)
-                distances.append([distance, category])
-        categories = [category[1] for category in sorted(distances)[:self.k]]
-        result = Counter(categories).most_common(1)[0][0]
-        return result
-    
-    def visualize(self):
-        fig = plt.figure(figsize=(15,12))
+                distances.append([distance, self.y[i]])
+            
+            categories = [category[1] for category in sorted(distances)[:self.k]]
+            result = Counter(categories).most_common(1)[0][0]
+            y_pred.append(result)
+        
+        return y_pred
+
+
+        
+    def visualize(self, X_test):
+        fig = plt.figure(figsize=(15, 12))
         ax = fig.add_subplot(projection="3d")
         ax.grid(True, color="#323232")
         ax.set_facecolor("gray")
@@ -37,23 +55,34 @@ class Knn:
         ax.tick_params(axis="x", color="white")
         ax.tick_params(axis="y", color="white")
 
-        for point in self.points["blue"]:
-            ax.scatter(point[0], point[1],point[2], color="#104DCA", s=60)#buraya useniom yarın yapcm
+        for i, point in enumerate(self.X):
+            if self.y[i] == "blue":
+                clr = "blue"
+            else:
+                clr = "red"
+            ax.scatter(point[0], point[1], point[2], color=clr, s=60)
 
-        for point in self.points["red"]:
-            ax.scatter(point[0], point[1],point[2], color="#FF0000", s=60)
+        for new_point in X_test:
+            check = self.predict(new_point)
+            clr = "#FF0000" if check == "red" else "#104DCA"
+            ax.scatter(new_point[0], new_point[1], new_point[2], color=clr, marker="*", s=200, zorder=100)
 
-        check = self.predict(new_point)
-        color = "#FF0000" if check == "red" else "#104DCA"
-
-        ax.scatter(self.new_point[0], self.new_point[1],self.new_point[2], color=color, marker="*", s=200, zorder=100)
-
-        for point in points["blue"]:
-            ax.plot([self.new_point[0], point[0]], [self.new_point[1], point[1]],[self.new_point[2], point[2]], color="#104DCA", linestyle="--", linewidth=1)
-
-        for point in points["red"]:
-            ax.plot([self.new_point[0], point[0]], [self.new_point[1],point[1]],[self.new_point[2], point[2]], color="#FF0000", linestyle="--", linewidth=1)
+            for c, point in enumerate(self.X):
+                if y[c] == "blue":
+                   clr ="blue"
+                else:
+                   clr ="red"
+                ax.plot([new_point[0], point[0]], [new_point[1], point[1]], [new_point[2], point[2]], color=clr, linestyle="--", linewidth=1)
 
         plt.show()
+
+
+
+clf = Knn(3)
+clf.fit(X,y)
+print(clf.predict(X_test))
+clf.visualize(X_test)
+y_pred = clf.predict(X_test)
+print("Tahmin edilen sınıflar:", y_pred)
 
 
